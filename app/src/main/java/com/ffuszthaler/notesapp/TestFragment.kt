49c67d4425e5bfceb493bbc095523e3a46ea3bb7
ffuszthaler <flo.fuszthaler@gmail.com>
2022-11-17 13:30:01 +0100

    init

---
+++ app/src/main/java/com/ffuszthaler/notesapp/TestFragment.kt
@@ -0,0 +1,29 @@
+package com.ffuszthaler.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class TestFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_test, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val testText = view.findViewById<TextView>(R.id.fragmentTestText)
+        testText.text = "hi"
+    }
+}
